2e4f3d3cb63a9d4abdb0241b2e919429f4ffe88b
pnabilah <puti.nabilah@ui.ac.id>
2026-03-15 09:22:49 +0700

    add mask_zero parameter to Embedding layer for better handling of padding

--- src/movie_sentiment_analysis/models.py
+++ src/movie_sentiment_analysis/models.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Embedding, GRU, Dense
 def build_gru_lang_binary(text_vec_layer, vocab_size, embed_dim=128, gru_units=128):
     model = Sequential([
         text_vec_layer,
-        Embedding(input_dim=vocab_size, output_dim=embed_dim),
+        Embedding(input_dim=vocab_size, output_dim=embed_dim, mask_zero=True),
         GRU(units=gru_units),
         Dense(units=1, activation="sigmoid")                        # 1 (positive) or 0 (negative)
     ])
